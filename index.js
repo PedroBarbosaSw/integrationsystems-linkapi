bd32bb2695df54e656085a4e6185c0533a66a825
Pedro Barbosa <pedro_dith@hotmail.com>
2021-10-05 21:48:48 -0300

    add pipedrive

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require('express')
+const lib = require('pipedrive')
 
 const app = express()
-const port = 8080
+const PORT = 8080
 
-app.get('/', (req , res) => {
-   res.send('Funcionou!')
-})
+lib.Configuration.apiToken = '16f06a7e97c6f2488818f4022174294e4b7c8159'
 
-app.listen(port, () => {
-   console.log(`Server is up and running at http://localhost:${port}`)
+app.get('/', async (req , res) => {
+   const user = await lib.UsersController.getCurrentUserData()
+   
+   res.send(user)   
+});
+app.listen(PORT, () => {
+   console.log(`Server is up and running at http://localhost:${PORT}`)
 })
